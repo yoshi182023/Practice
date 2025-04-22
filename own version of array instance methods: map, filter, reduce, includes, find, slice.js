00c8c655bb109c64cefdd87764d22eaff4f0558b
zzse <125266206+yoshi182023@users.noreply.github.com>
2025-04-22 10:29:06 -0700

    Update and rename own version of these array instance methods: map, filter, reduce, includes, find, slice.js to own version of array instance methods: map, filter, reduce, includes, find, slice.js

--- own version of array instance methods: map, filter, reduce, includes, find, slice.js
+++ own version of array instance methods: map, filter, reduce, includes, find, slice.js
@@ -29,3 +29,27 @@ console.log(arr.myMap(makeUpperCase)); // ['ABC', 'DEF', 'GHI']
 // 输出:
 // 'a', 0, ['a', 'b']
 // 'b', 1, ['a', 'b']
+
+
+Array.prototype.myFilter = function(callback, thisArg) {
+ const filteredArr = [];
+  for (let i = 0; i < this.length; i++) {
+    if (i in this) {
+
+      // 调用callback，传入当前元素、索引和原数组
+      // 使用thisArg作为callback的this值
+      if (callback.call(thisArg, this[i], i, this)) {
+        filteredArr.push(this[i]);
+      }
+    }
+  }
+  return filteredArr;
+}
+const numbers = [1, 2, 3, 4, 5,6];
+const evenNative = numbers.filter(x => x % 2 === 0);
+console.log(evenNative); // [2, 4]
+// 使用自定义myFilter
+const evenCustom = numbers.myFilter(x => x % 2 === 0);
+console.log(evenCustom); // [2, 4]
+
+
